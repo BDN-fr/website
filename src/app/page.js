0bc349799bc237d020d0e574ebe41d864bc17d5c
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-04-02 15:56:18 +0200

    Reworking "/" and adding "/test"

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,21 @@ import './page.css'
 
 import { headers } from 'next/headers';
 
-export default async function Home() {
+async function WelcomeTitle() {
     var host = (await headers()).get('host')
     return (
-        <main>
+        <div className='h-centered'>
             <h1 id="welcome" className="title">Welcome to</h1>
-            <a href="/" id="host" className="title"><i>{host}</i></a>
+            <a href="/" id="host" className="title">{host}</a>
             <p>Testing if this commit is automaticly built</p>
+        </div>
+    )
+}
+
+export default function Home() {
+    return (
+        <main>
+            <WelcomeTitle/>
         </main>
     );
 }
