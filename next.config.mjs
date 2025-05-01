86e40248dcfde11b87c53a3b9a60568f3fededb3
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-05-01 20:42:29 +0200

    Add docker compatibility (encore)

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+/** @type {import('next').NextConfig} */
+module.exports = {
+    output: "standalone",
+};
 export default nextConfig;
