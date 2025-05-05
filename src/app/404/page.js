9878599ed38bf93f3b5819a214adcb57b654d35b
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-05-06 00:34:45 +0200

    fix consoleEasterEgg, rework 404

--- src/app/404/page.js
+++ src/app/404/page.js
@@ -1,9 +1,57 @@
+'use client'
+
+import { useEffect } from 'react'
+import './404.css'
+
 export default function Page() {
+    useEffect(() => {
+        var [diffX, diffY] = [0, 0]
+
+        // Made with the help of this post https://stackoverflow.com/a/9334106
+
+        document.getElementById('message').addEventListener('mousedown', mouseDown, false)
+        window.addEventListener('mouseup', mouseUp, false)
+
+        function mouseUp() {
+            window.removeEventListener('mousemove', divMove, true)
+        }
+
+        function mouseDown(e) {
+            window.addEventListener('mousemove', divMove, true)
+            var divBCR = document.getElementById('message').getBoundingClientRect()
+            diffX = e.clientX - divBCR.x
+            diffY = e.clientY - divBCR.y
+        }
+
+        function divMove(e){
+            var div = document.getElementById('message')
+            var divBCR = document.getElementById('message').getBoundingClientRect()
+            var bodyBCR = document.body.getBoundingClientRect()
+            var [newX, newY] = [e.clientX - diffX, e.clientY - diffY + scrollY]
+            if (divBCR.right >= bodyBCR.width) {
+                newX = bodyBCR.width - divBCR.width
+            }
+            if (newX < 0) {
+                newX = 0
+            }
+            console.log(bodyBCR.bottom)
+            if (divBCR.bottom >= bodyBCR.bottom) {
+                newY = bodyBCR.height - divBCR.height
+            }
+            if (newY < 0) {
+                newY = 0
+            }
+            div.style.left = newX + 'px'
+            div.style.top = newY + 'px'
+        }
+    })
+
     return (
         <main className="fully-centered">
-            <div className="bordered">
+            <div className="bordered" id='message'>
                 <h1>404</h1>
-                <h1>(Really) nothing to see here (for now)</h1>
+                <h1>Oops... You're lost !</h1>
+                <p>No problem, there is a cat picture for you</p>
                 <h2>Here is the <a href="/">Homepage</a></h2>
             </div>
         </main>
--- src/components/consoleEasterEgg.js
+++ src/components/consoleEasterEgg.js
@@ -1,13 +1,12 @@
 'use client'
 
-export function ConsoleEaterEgg() {
-    console.log(
-`Hi dear console oppener or source code explorer, have a great day :3
+import { useEffect } from "react"
 
-|‾‾‾‾‾‾\\ |‾‾‾‾‾\\  |‾\\    |
-|      / |      | |  \\   |        /‾‾‾‾‾ |‾‾‾‾\\
-|‾‾‾‾‾‾\\ |      | |   \\  |        |‾‾‾‾  |____/
-|______/ |_____/  |    \\_| ______ |      |    \\
-`
-    )
+export function ConsoleEaterEgg() {
+    useEffect(() => {
+        console.log(`Hi dear console oppener or source code explorer, have a great day :3`)
+        setTimeout(() => {
+            console.log('You\'re still there ? There is not much to see to be honest...')
+        }, 30000);
+    })
 }
