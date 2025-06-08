7e81b77475ebf1941060b5cc798918311501ab36
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-06-08 15:48:42 +0200

    Add analytics (Umami)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,11 +8,13 @@ import Footer from '@/components/footer'
 import ConsoleEaterEgg from '@/components/consoleEasterEgg'
 
 import '@/global.css'
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Script src='https://analytics.bdn-fr.xyz/script.js' data-website-id="11f9c6a0-b6cd-4a26-9edf-1c1518b465a0"/>
         <Header/>
         {children}
         <Footer/>
