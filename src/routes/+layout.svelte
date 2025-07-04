8ff2bed5532dbe1cb938e1a534abd117459b7bbd
BDN_fr <lbrpxbdn@gmail.com>
2025-07-04 21:28:46 +0200

    change some css and the font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-neutral-300 dark:bg-neutral-800 text-black dark:text-white">
-  <nav class="flex items-center h-[5vh] absolute mx-auto px-4 top-2 left-0 right-0 w-2/3 bg-neutral-500/70 rounded-[25px] backdrop-blur-md gap-2">
+<div class="bg-neutral-100 dark:bg-neutral-800 text-black dark:text-white">
+  <nav class="flex items-center h-[5vh] absolute mx-auto px-4 top-2 left-0 right-0 w-2/3 bg-neutral-500/70 rounded-full backdrop-blur-md gap-2">
     <p>More content soon :)</p>
     <div class="absolute right-4 h-[80%]">
       <ThemeSwitcher/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-  <div class="flex items-center justify-center flex-col lg:flex-row h-svh gap-5 text-center p-5">
-    <img src="/images/profile.png" alt="My profile" class="h-50 rounded-[25px] border-white border-2 border-solid"/>
+  <div id="intro" class="flex items-center justify-center flex-col lg:flex-row h-svh gap-5 text-center p-5">
+    <img src="/images/profile.png" alt="My profile" class="h-50 rounded border-white border-2 border-solid"/>
     <div>
       <h1 class="text-7xl">Hello world !</h1>
       <p>Welcome to my awesome website !</p>
