4b1d188b3eeef1a67f9b846187f5e1f54963c9b5
BDN-fr <98167323+BDN-fr@users.noreply.github.com>
2025-07-20 03:54:00 +0200

    Fix Halo color

--- src/lib/components/ui/Halo.svelte
+++ src/lib/components/ui/Halo.svelte
@@ -1,9 +1,9 @@
 <script>
-  let { children, color='--custom-blue-200', round=false, onhover=false } = $props()
+  let { children, colorClass='bg-(--custom-blue-200)', round=false, onhover=false } = $props()
 </script>
 
 <div class="relative flex items-center justify-center size-full">
-  <div class="absolute {color.startsWith('--') ? `bg-(${color})` : `bg-[${color}]`} {round ? 'rounded-full' : ''} size-full blur-2xl {onhover ? 'hidden' : 'block'}"></div>
+  <div class="absolute {colorClass} {round ? ' rounded-full' : ''} size-full blur-2xl {onhover ? 'hidden' : 'block'}"></div>
   <div class="relative z-10 size-full">
     {@render children()}
   </div>
