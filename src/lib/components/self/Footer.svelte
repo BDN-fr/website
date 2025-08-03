4abcdf95f4d40945328b30ba25252690deb1ee39
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-08-03 12:55:17 +0200

    Add cookies message

--- src/lib/components/self/Footer.svelte
+++ src/lib/components/self/Footer.svelte
@@ -7,4 +7,5 @@
   <h1 class="text-xl">{m.WIP()} <span class="text-xs">{m.footer_forever()}</span></h1>
   <Link href="http://github.com/BDN-fr/website/">{m.source_code()}</Link>
   <Link href="/noskid.png">{m.not_a_skid()}</Link>
+  <p class="text-xs">{m.cookies_message()}</p>
 </footer>
