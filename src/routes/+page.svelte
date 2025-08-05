bf0c80584315718a5ebbd09c52e5124c18d32dd6
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-08-05 21:18:19 +0200

    Update Hero header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,14 @@
   import HeroHeader from "./sections/HeroHeader.svelte";
 	import { m } from "$lib/paraglide/messages";
 	import Contact from "./sections/Contact.svelte";
-	import Navbar from "$lib/components/self/Navbar.svelte";
 	import Seo from "$lib/components/self/SEO.svelte";
 	import Socials from "./sections/Socials.svelte";
 </script>
 
 <Seo title={m.title_homepage()} />
 
-<Navbar/>
 <HeroHeader/>
-<main class="flex flex-col items-center text-center gap-48 w-[95%] lg:w-[80%] mx-auto">
+<main class="flex flex-col items-center text-center gap-32 w-[95%] lg:w-[80%] mx-auto">
   <Skills/>
   <Projects/>
   <Contact/>
