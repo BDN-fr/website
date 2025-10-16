c8737d93e99395e09d4e2730a70e96b0da037836
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-10-16 13:30:30 +0200

    Add about me (need to make the translation but whatever)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,14 @@
 	import Contact from './sections/Contact.svelte';
 	import Seo from '$lib/components/self/SEO.svelte';
 	import Socials from './sections/Socials.svelte';
+	import AboutMe from './sections/AboutMe.svelte';
 </script>
 
 <Seo title={m.title_homepage()} />
 
 <HeroHeader />
 <main class="mx-auto flex w-[95%] flex-col items-center gap-32 text-center lg:w-[80%]">
+	<AboutMe />
 	<Skills />
 	<Projects />
 	<Contact />
---
+++ src/routes/sections/AboutMe.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Link from "$lib/components/ui/Link.svelte";
+	import Subsection from "$lib/components/ui/Subsection.svelte";
+	import { m } from "$lib/paraglide/messages";
+
+  let dob = new Date('03/10/2008')
+  let today = new Date()
+  let age = new Date(today.getTime() - dob.getTime()).getFullYear() - new Date(0).getFullYear()
+</script>
+
+<Subsection title={m.about_me()}>
+  <div class="flex justify-center md:w-2/3 mx-auto">
+    <p class="text-left">
+      I'm a {age} yo transgender 🏳️‍⚧️ non-binary girl nerd who likes eating graphic cards and play video games.
+      My pronoums are she / they.
+      I live in France in the Nord, I'm in terminale (12th grade in the country of guns and eagles) with Maths and Computer class in specialites.
+      I learnt developpement by myself starting with python at the age of 12 (not sure tbh xD).
+      I really don't like Javascript but (of course) like Python.
+      I prefer backend but have no problem doing frontend.
+      I self-host my website and services with a VPS (which I pay like 5€/month) and <Link href="https://dokploy.com/">Dokploy</Link>.
+    </p>
+  </div>
+</Subsection>
