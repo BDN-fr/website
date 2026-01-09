e45fd57857f1dc92ed56c0e056104a7ade07c8c4
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2026-01-09 21:17:59 +0100

    uhhhhh

---
+++ src/lib/components/apps/Clicker.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Window from "../ui/Window.svelte";
+  import Button from "../ui/Button.svelte";
+
+  interface Props {
+    windowOpened: boolean;
+  }
+  let { windowOpened = $bindable(false) }: Props = $props()
+	let count = $state(0);
+</script>
+
+<Window
+	title={`Clicker | ${count}`}
+	bind:open={windowOpened}
+	class="flex flex-col gap-2 items-center justify-center p-8 px-16"
+	minWidthClass="min-w-fit"
+	minHeightClass="min-h-fit"
+	isResizable={false}
+	isFullscreenable={false}
+>
+	<Button onclick={() => (count += 1)}>Click</Button>
+</Window>
---
+++ src/lib/components/self/Desktop/Desktop.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Clicker from "$lib/components/apps/Clicker.svelte";
+import Icon from "./Icon.svelte";
+
+  let clickerWindow = $state(false)
+</script>
+
+<Clicker bind:windowOpened = {clickerWindow} />
+
+<div class="min-w-svw max-w-lvw min-h-svh max-h-lvh gap-4 p-4 flex flex-col flex-wrap place-content-start">
+  <Icon appName="Clicker" onOpen={() => {clickerWindow = true}}>
+    <span class="icon-[pixel--trending-solid]"></span>
+  </Icon>
+</div>
